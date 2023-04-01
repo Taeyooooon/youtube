188cb211d4077e9d19aeab9712040c37ec3cc0ec
Taeyooooon <eric568@naver.com>
2023-04-01 22:42:46 +0900

    Refactor: youtube api 의존성 주입 리팩토링

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,11 +1,12 @@
 import { createContext, useContext } from 'react';
-import FakeYoutube from '../api/fakeYoutube';
 import Youtube from '../api/youtube';
+import FakeYoutubeClient from '../api/fakeYoutubeClient';
 
 export const YoutubeApiContext = createContext();
 
-const youtube = new FakeYoutube();
-// const youtube = new Youtube();
+const client = new FakeYoutubeClient();
+// const client = new YoutubeClient();
+const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }) {
   return (
